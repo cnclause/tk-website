9a23dc28fe9526f6e4c3973cebe8ef01b37d8a70
github username <github email address>
2020-03-27 14:07:01 -0600

    have our story placed on avout us page

---
+++ client/src/components/AboutUs/AboutUsContainer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class='about-us-container'>
+       <OurStory />
+    </div>
+</template>
+
+<script>
+import OurStory from './OurStory.vue'
+
+export default {
+    components:{
+        OurStory
+    }
+    
+}
+</script>
+
+<style>
+
+.about-us-container{
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+}
+
+
+</style>
---
+++ client/src/components/AboutUs/OurStory.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="our-story-container">
+        <h1 class="subtitle-1 our-story-title"> Our Story </h1>
+        <p class="body-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br> 
+        <br>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+    </div>
+</template>
+
+<style lang="scss">
+
+    .our-story-container{
+        width: 60%;
+    }
+
+    .our-story-title{
+        margin-bottom: 2rem;
+        margin-top: 3rem;
+        text-align: center;
+    }
+</style>
