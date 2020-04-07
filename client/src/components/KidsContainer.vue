573b889674f88e174e2326ea636258a7b9694024
ParkerBacall <pbacall2@gmail.com>
2020-04-07 13:05:16 -0600

    hook up compoents

--- client/src/components/KidsContainer.vue
+++ client/src/components/KidsContainer.vue
@@ -1,11 +1,16 @@
 <template>
  <div class="kids-container">
-    <h1 class="title">Meet the Kids</h1>
+    <h1 class="title">Meet the Birds</h1>
+    <MeetBirdsContainer/>
   </div>
 </template>
 
 <script>
+  import MeetBirdsContainer from './MeetBirds/MeetBirdsContainer'
 export default {
-    name: 'kids'
+    name: 'kids',
+    components: {
+      MeetBirdsContainer
+    }
 }
 </script>
--- client/src/components/MeetBirds/MeetBirdsContainer.vue
+++ client/src/components/MeetBirds/MeetBirdsContainer.vue
@@ -1,3 +1,3 @@
 <template>
-    <h1> Meeth the Birds </h1>
+    <h1> Meet the Birds </h1>
 </template>
