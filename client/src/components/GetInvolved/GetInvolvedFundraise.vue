de181e5ff28385b915edd20f90e8b6fbc2b248df
github username <github email address>
2020-04-13 17:56:18 -0600

    added card for fundraise and containers

--- client/src/components/GetInvolved/GetInvolvedFundraise.vue
+++ client/src/components/GetInvolved/GetInvolvedFundraise.vue
@@ -1,7 +1,23 @@
 <template>
     <div class="get-involved-fundraise">
-        <v-card>
-            <v-card-title> Fundraise</v-card-title>
+        <v-card outlined class="get-involved-fundraise-card" flat>
+            <h1 class="get-involved-fundraise-title" :class="[$vuetify.breakpoint.mdAndUp ? 'body-1' : 'body-2']"> Fundraise</h1>
         </v-card>
     </div>
 </template>
+
+<style>
+    .get-involved-fundraise{
+        width: 80%;
+        display: flex;
+        justify-content: center;
+    }
+
+    .get-involved-fundraise-card{
+        width: 40%;
+    }
+
+    .get-involved-fundraise-title{
+        text-transform: uppercase;
+    }
+</style>
