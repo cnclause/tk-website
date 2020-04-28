189f996904dacf6090685ce62d68f2ac2a017d5a
github username <github email address>
2020-04-28 14:28:26 -0600

    changed meet the kids links to aws and started to great staff page

--- client/src/components/MeetBirds/MeetBirdsContainer.vue
+++ client/src/components/MeetBirds/MeetBirdsContainer.vue
@@ -5,7 +5,7 @@
                 <h1 class ="meet-birds-title" :class="[$vuetify.breakpoint.mdAndUp ? 'display-4' : 'display-2']">Meet the Birds</h1>
             </div>
             <div class="meet-birds-title-img-container" :class="[$vuetify.breakpoint.mdAndUp ? 'large' : 'small']">
-                <v-img class="meet-birds-title-img" src="https://lh3.googleusercontent.com/lfG2xjPpzO-IwFKcmTfrpihQiq6OJ3dVtEvPx6mtZn1WDJlLGfSuqp56cMpwv463FGD9MRFPm1PZrxl3ehY8RSuZa2hNA7ZQqGURDIFF9XgfPd0mRt5mn3MVF5umqpLyTcMZSoVCBd0O6kZs7mAVVLcQoZgyIRQjKvhNlgeaP21YADdTft-AewKEEMlMOhb853J9KYN_j7NmBk_gzzRkfbFCYfU1sHHldgjiodS5W4_xsQ6E7kyvHn8IVpO7pNn6lEuQwNY_x-XkijC49wAUaTIw4WJmEQ13LD8uTt2qp5wtMNg1qaE994xCHKcUjNaRlaqmZPBWjd9vb1qDwQ53_ptGiP3xUWbjPGqaJ1NQEBKFIqN93lCD3dllhqhXhMlQkucMFD9VyT5sZlGYmXhontPvbqZ_ATEdRX6OpPnijt4hRnuZ1YZCY_lLTEAe0mNJywzGQNxB2V1hoJA5Nx2zo8yN0yaDh6Z-yFfWC4jjR1f63UkyxQHBGIhOd4UP2rz0axkqORA5IcJd5IMjjAHGxwjS2J-cSK4nunHKB6Nqx82ooaYupC2drrk3uv114AxShkQH_m2i3199qw3d5y6MakzbriJxF5MX9j-xnlsmvwuhVV9a0UV2QZwSMUrpq1ImTxT-ZNz4hFxAwj_ZUb1tORoYQIji-YlDZZjFHW7mI-DTB7vDDdTIBw4utgEniUQbn4cAePMA9k6yqHsfB9biIXUIo8nK8YlnyRnEXlz2TCFYB1yE00JbJkM=w2198-h1236-no"
+                <v-img class="meet-birds-title-img" src="https://rootstowingspictures.s3-us-west-1.amazonaws.com/TK+Logo+Transparent+Background+no+words-01+(1).png"
                     max-height="40rem"
                     contain
                 ></v-img>
