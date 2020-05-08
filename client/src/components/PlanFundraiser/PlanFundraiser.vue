b5c73de84b6b903d526a8f3df39fd8964de823fb
github username <github email address>
2020-05-08 12:08:42 -0600

    set up fundraise with us page with subehader

--- client/src/components/PlanFundraiser/PlanFundraiser.vue
+++ client/src/components/PlanFundraiser/PlanFundraiser.vue
@@ -1,11 +1,48 @@
 <template>
-    <h1>Frundraiser</h1>
+    <div class="plan-fundraiser">
+    <div class="plan-fundraiser-container">
+      <h1 class ="plan-fundraiser-title" :class="[$vuetify.breakpoint.mdAndUp ? 'display-4' : 'display-2']">Plan a Fundraiser</h1>
+    </div>
+    <PlanFundraiserSubHeader />
+  </div>
 </template>
 
 <script>
+import PlanFundraiserSubHeader from './PlanFundraiserSubHeader'
 
 export default {
     name: 'plan',
+    components: {
+        PlanFundraiserSubHeader
+    }
 }
 
 </script>
+
+<style lang="scss">
+  .plan-fundraiser-container{
+    margin: 2rem;
+    z-index: 0;
+    height: 35rem;
+    background-image: url('https://rootstowingspictures.s3-us-west-1.amazonaws.com/ArtShow-ChristysTable.jpg');
+    background-position: center; /* Center the image */
+    background-repeat: no-repeat; /* Do not repeat the image */
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    }
+
+    .plan-fundraiser-title{
+        color: white;
+        // text-align: center
+    }
+
+    .plan-fundraiser-title.display-4{
+        margin-left: 5rem;
+        padding-top: 3rem !important;
+    }
+
+    .plan-fundraiser-title.display-2{
+        font-size: 40pt;
+    }
+</style>
---
+++ client/src/components/PlanFundraiser/PlanFundraiserSubHeader.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="plan-fundraiser-subheader-container">
+        <div :class="[$vuetify.breakpoint.mdAndUp ? 'body-1' : 'body-2']" class='plan-fundraiser-subheader'> 
+            Take a lead and change <strong>Everything</strong></div>
+        <div :class="[$vuetify.breakpoint.mdAndUp ? 'body-1' : 'body-2']"       class='plan-fundraiser-subheader-info'>
+            Organized fundraisers are a primary way From Roots to Wings funds its programs and activities. We have teams that organize and execute fundraisers in many cities across the United States. You can lead a team. Contact us to learn how!
+        </div>
+        <v-btn color="green white--text" depressed rounded class="plan-fundraiser-subheader-btn" to="/donate"> Contact Us</v-btn>
+    </div>
+</template>
+
+<style>
+  .plan-fundraiser-subheader-container{
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+  }
+
+  .plan-fundraiser-subheader{
+    margin-top: 3rem;
+  }
+
+  .plan-fundraiser-subheader.body-1{
+    text-align: center;
+    text-transform: uppercase;
+    letter-spacing: 0.3rem !important;
+    font-size: 20pt !important;
+  }
+
+  .plan-fundraiser-subheader.body-2{
+    text-align: center;
+    font-size: 16pt !important;
+    margin: 1rem;
+  }
+
+
+  .plan-fundraiser-subheader-info.body-1{
+    width: 60%;
+    text-align: center;
+    margin-top: 1rem;
+    font-size: 15pt !important;
+  }
+
+  .plan-fundraiser-subheader-info.body-2{
+    text-align: center;
+    margin-top: 0.5rem;
+  }
+
+  .plan-fundraiser-subheader-btn{
+    letter-spacing: 0.4rem;
+    font-size: 15pt !important;
+    margin-top: 2rem;
+    font-weight: 400;
+  }
+
+</style>
